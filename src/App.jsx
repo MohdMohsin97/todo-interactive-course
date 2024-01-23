57e31985417723893c4e0c43cdcc215d53af8bb4
MohdMohsin97 <mohsinmohd9718@gmail.com>
2024-01-23 17:23:11 +0530

    add Add and Delete button

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ export default function App() {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
 
+  // Create a function addTodo which add new object to todos with id as Date, text as newTodo and set newTodo to empty string
+  const addTodo = () => {};
+
+  // Create a function with take id as params and delete it todo id from todos
+  const deleteTodo = (id) => {};
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">Todo App</h1>
@@ -16,10 +22,7 @@ export default function App() {
           placeholder="Add a new Todo"
           className="border p-2 flex-grow"
         />
-        <button
-          onClick={() => console.log("clicked")}
-          className="bg-blue-500 text-white p-2"
-        >
+        <button onClick={addTodo} className="bg-blue-500 text-white p-2">
           Add
         </button>
       </div>
@@ -30,6 +33,7 @@ export default function App() {
             className="flex justify-between items-center space-x-2 mb-4"
           >
             <p className="text-xl">{todo.text}</p>
+            {/* Add delete function which call deleteTopo on click take todo is as argument */}
           </li>
         ))}
       </ul>
