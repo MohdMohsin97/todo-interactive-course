7a248a0ceffeccdf78dceeb07b6121d6039010fd
MohdMohsin97 <mohsinmohd9718@gmail.com>
2024-01-23 17:17:18 +0530

    add Add and Delete button

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
 import { useState } from "react";
 
-function App() {
-  // Create a const newTodo using useState with empty array as initial value
-
-  // Create a const newTodo using useState with empty string as initial value
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
 
   return (
     <div className="container mx-auto p-4">
-      {/* Title Todo App  */}
-
-      {/* Div Element with input element and Add button */}
-
+      <h1 className="text-3xl font-bold mb-4">Todo App</h1>
+      <div className="flex space-x-2 mb-4">
+        <input
+          type="text"
+          name=""
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Add a new Todo"
+          className="border p-2 flex-grow"
+        />
+        <button
+          onClick={() => console.log("clicked")}
+          className="bg-blue-500 text-white p-2"
+        >
+          Add
+        </button>
+      </div>
+      <ul className="list-disc pl-4 w-96 ">
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            className="flex justify-between items-center space-x-2 mb-4"
+          >
+            <p className="text-xl">{todo.text}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
